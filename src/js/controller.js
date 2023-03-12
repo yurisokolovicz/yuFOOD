d9f6249f73228467805013440915be3a4550666e
yurisokolovicz <yurisokolovicz@gmail.com>
2023-03-12 16:41:50 -0300

    Implementing Search Results - Part 2
    In this commit I have implemented the load spinner in the .results (the place where should appear the searched recipes. Also the render for searched recipes is working after the implementation in the resultsView.js. And the creation of error msg propertie at resultsView.js similar with the one at recipeView.js. The error msg was set to render in the View.js. In general togheter with the last commit I implemented the event that the user searches, after the async loaded the search results and then render it to our user interface as the description of the flowchart-part-1

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,11 +1,16 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
 //// Polyfilling - for the web app work in old browsers
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import recipeView from './views/recipeView.js';
+
+if (module.hot) {
+    module.hot.accept();
+}
 
 const recipeContainer = document.querySelector('.recipe'); // already in recipeView #################
 
@@ -32,6 +37,8 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
     try {
+        resultsView.renderSpiner();
+
         // 1) Get search query
         const query = searchView.getQuery();
         if (!query) return;
@@ -40,7 +47,8 @@ const controlSearchResults = async function () {
         await model.loadSearchResults(query);
 
         // 3) Render results
-        console.log(model.state.search.results);
+        // console.log(model.state.search.results);
+        resultsView.render(model.state.search.results);
     } catch (err) {
         console.log(err);
     }
--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,20 +1,20 @@
 class SearchView {
-    #parentEl = document.querySelector('.search');
+    _parentEl = document.querySelector('.search');
 
     getQuery() {
-        const query = this.#parentEl.querySelector('.search__field').value;
-        this.#clearInput();
+        const query = this._parentEl.querySelector('.search__field').value;
+        this._clearInput();
         return query;
     }
 
     // Clear field after searching
     // With # the method is private, it will not be acessed by outside of this file
-    #clearInput() {
-        this.#parentEl.querySelector('.search__field').value = '';
+    _clearInput() {
+        this._parentEl.querySelector('.search__field').value = '';
     }
 
     addHandlerSearch(handler) {
-        this.#parentEl.addEventListener('submit', function (e) {
+        this._parentEl.addEventListener('submit', function (e) {
             e.preventDefault();
             handler();
         });
