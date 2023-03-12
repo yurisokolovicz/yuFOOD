164ad271404e6f9ed59b5e2ba41aa1feee5f1291
yurisokolovicz <yurisokolovicz@gmail.com>
2023-03-12 10:46:43 -0300

    Implementing Error and Success Messages
    updating the error and success msg in recipeView.js

--- src/js/controller.js
+++ src/js/controller.js
@@ -29,7 +29,7 @@ const controlRecipes = async function () {
         // const recipeView = new recipeView(model.state.recipe)
         recipeView.render(model.state.recipe);
     } catch (err) {
-        console.log(err);
+        recipeView.renderError();
     }
 };
 
--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -6,6 +6,8 @@ import { Fraction } from 'fractional';
 class RecipeView {
     #parentElement = document.querySelector('.recipe');
     #data;
+    #errorMessage = 'We could not find that recipe. Please try another one!';
+    #message = '';
 
     render(data) {
         this.#data = data;
@@ -18,7 +20,7 @@ class RecipeView {
         this.#parentElement.innerHTML = '';
     }
 
-    renderSpiner = function () {
+    renderSpiner() {
         const markup = `
       <div class="spinner">
         <svg>
@@ -26,9 +28,41 @@ class RecipeView {
         </svg>
       </div>
   `;
-        this.#parentElement.innerHTML = '';
+        this.#clear();
         this.#parentElement.insertAdjacentHTML('afterbegin', markup);
-    };
+    }
+
+    renderError(message = this.#errorMessage) {
+        const markup = `
+          <div class="error">
+            <div>
+              <svg>
+                <use href="${icons}#icon-alert-triangle"></use>
+              </svg>
+            </div>
+            <p>${message}</p>
+          </div>  
+    `;
+
+        this.#clear();
+        this.#parentElement.insertAdjacentHTML('afterbegin', markup);
+    }
+
+    renderMessage(message = this.#message) {
+        const markup = `
+          <div class="message">
+            <div>
+              <svg>
+                <use href="${icons}#icon-smile"></use>
+              </svg>
+            </div>
+            <p>${message}</p>
+          </div>  
+    `;
+
+        this.#clear();
+        this.#parentElement.insertAdjacentHTML('afterbegin', markup);
+    }
 
     addHandlerRender(handler) {
         ['hashchange', 'load'].forEach(ev => window.addEventListener(ev, handler));
