2ae0d7a803d027753da4f6f868d865d7b39ba81e
yurisokolovicz <yurisokolovicz@gmail.com>
2023-03-13 16:28:34 -0300

    Updating Recipe Servings
    In this commit a controlServings function was created in the controller.js. A new updateServings function was created in the model.js and it was called in the controller.js to update the servings. This function logic was to increase and decrease the amount of ingredients when the number of servings increase or decrease. An addEventListener was created in the recipeView.js with the name of addHandlerUpdateServings to listen to the buttons of decreasing and increasing servings. This was connected with the controller.js by recipeView.addHandlerUpdateServings() receiving the controlServings information: recipeView.addHandlerUpdateServings(controlServings). Finally, the handler() was called in the recipeView.js in the line 23 passing the updateTo.

--- src/js/config.js
+++ src/js/config.js
@@ -2,4 +2,4 @@
 // Uppercase API_URL because is a constant that will never change (common practice)
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
-export const RES_PER_PAGE = 15;
+export const RES_PER_PAGE = 10;
