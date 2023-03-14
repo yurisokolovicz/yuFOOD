5657a0a450778c93f23e8b9da984a66bd44aed57
yurisokolovicz <yurisokolovicz@gmail.com>
2023-03-14 16:35:45 -0300

    npm i fracty

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -2,7 +2,7 @@ import View from './View.js';
 
 import icons from 'url:../../img/icons.svg'; // Parcel 2
 // console.log(icons);
-// import { Fraction } from 'fractional';
+import fracty from 'fracty';
 // console.log(Fraction);
 
 // It is a child class of View.js
@@ -112,7 +112,7 @@ class RecipeView extends View {
                    <svg class="recipe__icon">
                     <use href="src/img/icons.svg#icon-check"></use>
                    </svg>
-                  <div class="recipe__quantity">${ing.quantity ? new Fraction(ing.quantity).toString() : ''}</div>
+                  <div class="recipe__quantity">${ing.quantity ? fracty(ing.quantity) : ''}</div>
                   <div class="recipe__description">
                     <span class="recipe__unit">${ing.unit}</span>
                     ${ing.description}
